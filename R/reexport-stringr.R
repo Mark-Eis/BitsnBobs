21979e1bd701ceb17ea3412ab50997d6e7729989
Mark-Eis <mark@markeisler.com>
2024-02-10 22:05:52 +0000

    add reexport-stringr.R; issue #12

---
+++ R/reexport-stringr.R
@@ -0,0 +1,26 @@
+# BitsnBobs R Package
+# Mark Eisler - Feb 2024
+# For general bits and bobs of code
+#
+# Requires R version 4.2.0 (2022-04-22) -- "Vigorous Calisthenics" or later
+#
+# reexport-stringr.R
+
+
+# ========================================
+
+#' @importFrom stringr boundary
+#' @export
+stringr::boundary
+
+#' @importFrom stringr coll
+#' @export
+stringr::coll
+
+#' @importFrom stringr fixed
+#' @export
+stringr::fixed
+
+#' @importFrom stringr regex
+#' @export
+stringr::regex
