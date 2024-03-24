84e7b000f4c7b4e741082a23612630f100be4c33
Mark-Eis <mark@markeisler.com>
2024-03-24 08:53:56 +0000

    edit degreeconvert.R

--- R/degreeconvert.R
+++ R/degreeconvert.R
@@ -8,87 +8,89 @@
 
 
 # ========================================
-#' Convert Degrees, Minutes and Seconds to Decimal Degrees
+#' Create Decimal Degrees Object
 #'
 #' @description
-#' Convert degrees, minutes and seconds to decimal degrees. 
+#' The function `decdeg()` is used to create (latitude or longitude) coordinate objects represented in decimal degrees. 
 #'
 #' @details
-#' `dms_to_decdeg()` is an S3 function that works with individual coordinates, latitude and longitude
-#' values paired in a named list (see examples) or with a longer list of coordinates.
+#' `decdeg()` is a generic S3 function. The default method works with a numeric (`double`) representing a
+#' coordinate of latitude or longitude in decimal degrees.
 #'
 #' @family degreeconvert
 #'
-#' @param object an object of class [`"degminsec"`][degminsec], representing a coordinate of latitude or longitude in
-#'   degrees, minutes and seconds or a list of "degminsec" objects.
+#' @param x numeric, representing a coordinate of latitude or longitude in decimal degrees.
 #'
 #' @inheritParams degminsec
 #'
-#' @return An object of class [`"decdeg"`][decdeg], representing a coordinate of latitude or longitude in decimal
-#'   degrees represented by a numeric of type `double` with maximum absolute value of 180, or a list of such objects.
+#' @return An object of class `"decdeg"`, representing a coordinate of latitude or longitude in decimal degrees
+#'   represented by a numeric of type `double` with maximum absolute value of 180.
 #'
 #' @keywords utilities
 #'
 #' @export
 #' @examples
-#'
-#' dms_to_decdeg(49.32464)
-#' dms_to_decdeg(49.32464, pointafter = "deg")
-#' dms_to_decdeg(4932.464, pointafter = "min")
-#' dms_to_decdeg(493246.4, pointafter = "sec")
-#'
-#' (coord <- degminsec(49.32464))
-#' dms_to_decdeg(coord)
-#'
-#' (coords <- list(lat = degminsec(49.32464), long = degminsec(18.2354822)))
-#' dms_to_decdeg(coords)
-#'
-#' rm(coord, coords)
+#' decdeg(49.54622)
 
-dms_to_decdeg <- function(object, ...) {
-    UseMethod("dms_to_decdeg")
+decdeg <- function(x, ...) {
+    UseMethod("decdeg")
 }
 
 # ========================================
-#  Convert Degrees, Minutes and Seconds in a "degminsec" object to Decimal Degrees
-#  S3method dms_to_decdeg.degminsec()
+#  Create Decimal Degrees Object
+#  S3method decdeg.default()
 #'
-#' @rdname dms_to_decdeg
+#' @rdname decdeg
 #' @export
 
-dms_to_decdeg.degminsec <- function(object, ...) {
+decdeg.default <- function(x, ...) {
     check_dots_empty()
-    validate_degminsec(object)
-    with(object, deg + min / 60 + sec / 3600) |>
-    decdeg()
+    new_decdeg(x) |> validate_decdeg()
 }
 
 # ========================================
-#  Convert Degrees, Minutes and Seconds to Decimal Degrees
-#  S3method dms_to_decdeg.default()
-#'
-#' @rdname dms_to_decdeg
-#' @export
+#  Constructor
+#  new_decdeg()
+#
+#  not exported
 
-dms_to_decdeg.default <- function(object, ..., pointafter = c("deg", "min", "sec")) {
-    check_dots_empty()
-    stopifnot(is.numeric(object))
-    degminsec(object, pointafter = pointafter) |>
-    dms_to_decdeg()
+new_decdeg <- function(d)
+    structure(d, class = "decdeg")
+
+# ========================================
+#  Validator
+#  validate_decdeg()
+#
+#  not exported
+
+validate_decdeg <- function(dec_deg) {
+
+    if (!inherits(dec_deg, "decdeg"))
+      stop(
+        "`dec_deg` must be of class \"decdeg\"",
+        call. = FALSE
+      )
+
+    if (abs(dec_deg) > 180)
+      stop(
+        "`dec_deg` must not be greater than 180",
+        call. = FALSE
+      )
+    
+    dec_deg
 }
 
 # ========================================
-#  Convert Degrees, Minutes and Seconds in a list to Decimal Degrees
-#  S3method dms_to_decdeg.list()
+# Print decdeg Object
+#  S3method print.decdeg()
 #'
-#' @rdname dms_to_decdeg
+#' @rdname decdeg
 #' @export
 
-dms_to_decdeg.list <- function(object, ...) {
-    check_dots_empty()
-    stopifnot(all(purrr::map_lgl(object, \(x) (inherits(x, "degminsec")))))
-    lapply(object, dms_to_decdeg)
-}
+print.decdeg <- function(x, ...) {
+    cat(paste("\n\t", zapsmall(x), "decimal degrees\n"))
+    invisible(x)
+} 
 
 # ========================================
 #' Create Degrees, Minutes and Seconds Object
@@ -217,86 +219,84 @@ print.degminsec <- function(x, ...) {
 } 
 
 # ========================================
-#' Create Decimal Degrees Object
+#' Convert Degrees, Minutes and Seconds to Decimal Degrees
 #'
 #' @description
-#' The function `decdeg()` is used to create (latitude or longitude) coordinate objects represented in decimal degrees. 
+#' Convert degrees, minutes and seconds to decimal degrees. 
 #'
 #' @details
-#' `decdeg()` is a generic S3 function. The default method works with a numeric (`double`) representing a
-#' coordinate of latitude or longitude in decimal degrees.
+#' `dms_to_decdeg()` is an S3 function that works with individual coordinates, latitude and longitude
+#' values paired in a named list (see examples) or with a longer list of coordinates.
 #'
 #' @family degreeconvert
 #'
-#' @param x numeric, representing a coordinate of latitude or longitude in decimal degrees.
+#' @param object an object of class [`"degminsec"`][degminsec], representing a coordinate of latitude or longitude in
+#'   degrees, minutes and seconds or a list of "degminsec" objects.
 #'
 #' @inheritParams degminsec
 #'
-#' @return An object of class `"decdeg"`, representing a coordinate of latitude or longitude in decimal degrees
-#'   represented by a numeric of type `double` with maximum absolute value of 180.
+#' @return An object of class [`"decdeg"`][decdeg], representing a coordinate of latitude or longitude in decimal
+#'   degrees represented by a numeric of type `double` with maximum absolute value of 180, or a list of such objects.
 #'
 #' @keywords utilities
 #'
 #' @export
 #' @examples
-#' decdeg(49.54622)
+#'
+#' dms_to_decdeg(49.32464)
+#' dms_to_decdeg(49.32464, pointafter = "deg")
+#' dms_to_decdeg(4932.464, pointafter = "min")
+#' dms_to_decdeg(493246.4, pointafter = "sec")
+#'
+#' (coord <- degminsec(49.32464))
+#' dms_to_decdeg(coord)
+#'
+#' (coords <- list(lat = degminsec(49.32464), long = degminsec(18.2354822)))
+#' dms_to_decdeg(coords)
+#'
+#' rm(coord, coords)
 
-decdeg <- function(x, ...) {
-    UseMethod("decdeg")
+dms_to_decdeg <- function(object, ...) {
+    UseMethod("dms_to_decdeg")
 }
 
 # ========================================
-#  Create Decimal Degrees Object
-#  S3method decdeg.default()
+#  Convert Degrees, Minutes and Seconds to Decimal Degrees
+#  S3method dms_to_decdeg.default()
 #'
-#' @rdname decdeg
+#' @rdname dms_to_decdeg
 #' @export
 
-decdeg.default <- function(x, ...) {
+dms_to_decdeg.default <- function(object, ..., pointafter = c("deg", "min", "sec")) {
     check_dots_empty()
-    new_decdeg(x) |> validate_decdeg()
+    stopifnot(is.numeric(object))
+    degminsec(object, pointafter = pointafter) |>
+    dms_to_decdeg()
 }
 
 # ========================================
-#  Constructor
-#  new_decdeg()
-#
-#  not exported
-
-new_decdeg <- function(d)
-    structure(d, class = "decdeg")
-
-# ========================================
-#  Validator
-#  validate_decdeg()
-#
-#  not exported
-
-validate_decdeg <- function(dd) {
-
-    if (!inherits(dd, "decdeg"))
-      stop(
-        "`dd` must be of class \"decdeg\"",
-        call. = FALSE
-      )
-
-    if (abs(dd) > 180)
-      stop(
-        "`dd` must not be greater than 180",
-        call. = FALSE
-      )
-    
-    dd
-}
-
-# ========================================
-# Print decdeg Object
-#  S3method print.decdeg()
+#  Convert Degrees, Minutes and Seconds in a "degminsec" object to Decimal Degrees
+#  S3method dms_to_decdeg.degminsec()
 #'
-#' @rdname decdeg
+#' @rdname dms_to_decdeg
 #' @export
 
-print.decdeg <- function(x, ...) {
-    cat(paste("\n\t", zapsmall(x), "decimal degrees\n"))
-    invisible(x)
-} 
+dms_to_decdeg.degminsec <- function(object, ...) {
+    check_dots_empty()
+    validate_degminsec(object)
+    with(object, deg + min / 60 + sec / 3600) |>
+    decdeg()
+}
+
+# ========================================
+#  Convert Degrees, Minutes and Seconds in a list to Decimal Degrees
+#  S3method dms_to_decdeg.list()
+#'
+#' @rdname dms_to_decdeg
+#' @export
+
+dms_to_decdeg.list <- function(object, ...) {
+    check_dots_empty()
+    stopifnot(all(purrr::map_lgl(object, \(x) (inherits(x, "degminsec")))))
+    lapply(object, dms_to_decdeg)
+}
